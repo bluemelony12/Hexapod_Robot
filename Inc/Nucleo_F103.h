74913ca7857156de24d09faa99510bdc3fab0c22
bluemelony12 <bluemelony12@gmail.com>
2018-11-26 15:47:43 +0900

    - 주석 수정

--- Inc/Nucleo_F103.h
+++ Inc/Nucleo_F103.h
@@ -1,19 +1,3 @@
-
-/*******************************************************************************
-  
-  * File name :  Nucleo_F103.h  
-  
-			* Application model : Nucleo_F103  Board  
- 
-			* Ver4.0
-			* January 23. 2017
-			* by Sang-min Kim, Jae-Il Kim  &  Dong-hyuk Cha
- 
- ****************************************************************************** */
-
-// -- <3> Nucleo_F103 보드의 사용에 필요한 내용을 정의, 선언한 헤더 파일
-
-// -- <3-1> Nucleo_F103 보드의 LED의 포트, 핀 번호를 정의 
 #define GPIONucleo					GPIOA 
 #define GPIOExt									GPIOC
 #define GPIO_PIN_Led1			GPIO_PIN_0
@@ -25,8 +9,6 @@
 #define GPIO_PIN_Led7			GPIO_PIN_6
 #define GPIO_PIN_Led8			GPIO_PIN_7
 #define GPIO_PIN_LedAll		GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7
-
-// -- <3-2> Nucleo_F103 보드의 Switch의  포트, 핀 번호를 정의
 
 #define GPIOSwNucleo							GPIOC
 #define GPIOSw1												GPIOA
@@ -41,7 +23,6 @@
 #define TIMER2													TIM2
 
 
-// -- <3-3> Nucleo_F103.c 에서 사용하는 함수의 선언
 void SystemClock_Config(void);
 void ServoPin_Config(void);
 void Sw_Config(void);
