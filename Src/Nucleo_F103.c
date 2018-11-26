5544dac728be8b1c5119a53e7cd2f9d136ba81d4
bluemelony12 <bluemelony12@gmail.com>
2018-11-26 15:36:33 +0900

    - 주석 수정

--- Src/Nucleo_F103.c
+++ Src/Nucleo_F103.c
@@ -1,21 +1,18 @@
-
-// -- <4-1> 프로그램의 실행에 필요한 헤더파일을 인클루드
 #include "main.h"
 #include "Nucleo_F103.h"
 
-// -- <4-2> 주변장치 초기화용 구조체
-GPIO_InitTypeDef		    						GPIO_Init_Struct; 					    // GPIO의 초기화용 구조체 변수 
+GPIO_InitTypeDef		    						GPIO_Init_Struct; 					    // GPIO의 초기화용 구조체 변수
 TIM_HandleTypeDef									TimHandle_4;
 TIM_HandleTypeDef									TimHandle_2;
-TIM_HandleTypeDef									TimHandle_3; 
+TIM_HandleTypeDef									TimHandle_3;
 TIM_OC_InitTypeDef								TIM_OCInit;												// 타이머 Channel(OC)의 초기화용 구조체 변수
-UART_HandleTypeDef						UartHandle;											// UART의 초기화용 구조체 변수 
-UART_HandleTypeDef						UartHandle_B;		
+UART_HandleTypeDef						UartHandle;											// UART의 초기화용 구조체 변수
+UART_HandleTypeDef						UartHandle_B;
 ADC_HandleTypeDef							AdcHandler;											// ADC의 초기화용 구조체 변수
 ADC_ChannelConfTypeDef   	Adc_sConfig; 									// ADC Channel의 초기화용 구조체 변수
 
 // -------------------------------------------------------------------------------- //
-// -- <5> Nucleo_F103 보드의 LED용 GPIO (PA5, PC0~PC7)의 초기설정을 함 
+// -- <5> Nucleo_F103 보드의 LED용 GPIO (PA5, PC0~PC7)의 초기설정을 함
 
 void Tim_Oc_It_Stop(void){
 	HAL_TIM_OC_Stop_IT(&TimHandle_2,TIM_CHANNEL_1);
@@ -34,122 +31,122 @@ void ServoPin_Config(void)
 		__HAL_RCC_GPIOA_CLK_ENABLE();
 		GPIO_Init_Struct.Pin = GPIO_PIN_All;
 		GPIO_Init_Struct.Mode  = GPIO_MODE_OUTPUT_PP;
-		GPIO_Init_Struct.Pull     = GPIO_PULLUP; 					
-		GPIO_Init_Struct.Speed = GPIO_SPEED_FREQ_HIGH;	
-	
+		GPIO_Init_Struct.Pull     = GPIO_PULLUP;
+		GPIO_Init_Struct.Speed = GPIO_SPEED_FREQ_HIGH;
+
 		HAL_GPIO_Init(GPIOA, &GPIO_Init_Struct);
-	
+
 		__HAL_RCC_GPIOC_CLK_ENABLE();
-		GPIO_Init_Struct.Pin = GPIO_PIN_All;	
+		GPIO_Init_Struct.Pin = GPIO_PIN_All;
 		GPIO_Init_Struct.Mode  = GPIO_MODE_OUTPUT_PP;
 		GPIO_Init_Struct.Pull     = GPIO_PULLUP;
 		GPIO_Init_Struct.Speed = GPIO_SPEED_FREQ_HIGH;
-	
-		HAL_GPIO_Init(GPIOC, &GPIO_Init_Struct); 
+
+		HAL_GPIO_Init(GPIOC, &GPIO_Init_Struct);
 }
 
 void Sw_Config(void)
 {
 		/*// -- Nucleo Board Switch (PC13)설정
-		// -- <6-1> GPIOC의 클럭을 enable		
-		__HAL_RCC_GPIOC_CLK_ENABLE();	
+		// -- <6-1> GPIOC의 클럭을 enable
+		__HAL_RCC_GPIOC_CLK_ENABLE();
 
 		// -- <6-2> SW1의 동작 조건을 GPIO 입력으로 설정
 		GPIO_Init_Struct.Pin 		= GPIO_PIN_13;	// GPIO에서 사용할 PIN 설정
 		GPIO_Init_Struct.Mode 	= GPIO_MODE_INPUT; 				// Input Floating 모드
 		GPIO_Init_Struct.Pull 		= GPIO_PULLUP; 						// Pull Up 모드
-		GPIO_Init_Struct.Speed 	= GPIO_SPEED_FREQ_HIGH; 		// 동작속도를 HIGH로 
+		GPIO_Init_Struct.Speed 	= GPIO_SPEED_FREQ_HIGH; 		// 동작속도를 HIGH로
 
-		// -- <6-3> GPIOC를 GPIO_Init_Struct에 설정된 조건으로 초기화함	
-		HAL_GPIO_Init(GPIOC, &GPIO_Init_Struct);    
+		// -- <6-3> GPIOC를 GPIO_Init_Struct에 설정된 조건으로 초기화함
+		HAL_GPIO_Init(GPIOC, &GPIO_Init_Struct);
 
 		// -- 확장 IO Board Switch (PA8, PB4, PB5, PB10)설정
 		//  SW1용 GPIO (PA8)의 초기설정
-		// -- <6-4> GPIOA의 클럭을 enable		
-		__HAL_RCC_GPIOA_CLK_ENABLE();	
+		// -- <6-4> GPIOA의 클럭을 enable
+		__HAL_RCC_GPIOA_CLK_ENABLE();
 
 		// -- <6-5> SW1의 동작 조건을 GPIO 입력으로 설정
 		GPIO_Init_Struct.Pin 		= GPIO_PIN_8;	// GPIO에서 사용할 PIN 설정
 		GPIO_Init_Struct.Mode 	= GPIO_MODE_INPUT; 				// Input Floating 모드
 		GPIO_Init_Struct.Pull 		= GPIO_PULLUP; 						// Pull Up 모드
-		GPIO_Init_Struct.Speed 	= GPIO_SPEED_FREQ_HIGH; 		// 동작속도를 HIGH로 
+		GPIO_Init_Struct.Speed 	= GPIO_SPEED_FREQ_HIGH; 		// 동작속도를 HIGH로
 
-		// -- <6-6> GPIOA를 GPIO_Init_Struct에 설정된 조건으로 초기화함	
-		HAL_GPIO_Init(GPIOA, &GPIO_Init_Struct);    
+		// -- <6-6> GPIOA를 GPIO_Init_Struct에 설정된 조건으로 초기화함
+		HAL_GPIO_Init(GPIOA, &GPIO_Init_Struct);
 
 		//  SW2, SW3, SW4용 GPIO (PB4, PB5, PB10)의 초기설정
-		// -- <6-7> GPIOB의 클럭을 enable	
-		__HAL_RCC_GPIOB_CLK_ENABLE();	
-		
+		// -- <6-7> GPIOB의 클럭을 enable
+		__HAL_RCC_GPIOB_CLK_ENABLE();
+
 		// -- <6-8> SW의 동작 조건을 GPIO 입력으로 설정
 		GPIO_Init_Struct.Pin 		= GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_10;		// GPIO에서 사용할 PIN 설정
 		GPIO_Init_Struct.Mode 	= GPIO_MODE_INPUT; 				// Input Floating 모드
 		GPIO_Init_Struct.Pull 		= GPIO_NOPULL; 						// Pull Up / Pull Down 비활성화
-		GPIO_Init_Struct.Speed 	= GPIO_SPEED_FREQ_HIGH; 		// 동작속도를 HIGH로 
+		GPIO_Init_Struct.Speed 	= GPIO_SPEED_FREQ_HIGH; 		// 동작속도를 HIGH로
 
-		// -- <6-9> GPIOB를 GPIO_Init_Struct에 설정된 조건으로 초기화함	
-		HAL_GPIO_Init(GPIOB, &GPIO_Init_Struct);   		 
-*/		
-		
+		// -- <6-9> GPIOB를 GPIO_Init_Struct에 설정된 조건으로 초기화함
+		HAL_GPIO_Init(GPIOB, &GPIO_Init_Struct);
+*/
+
 }
 
 // -------------------------------------------------------------------------------- //
-// -- <7> Nucleo_F103 보드의  Switch의 GPIO (PC13, PA8, PB4, PB5, PB10)의 초기설정을 함  
+// -- <7> Nucleo_F103 보드의  Switch의 GPIO (PC13, PA8, PB4, PB5, PB10)의 초기설정을 함
 //				* 이 함수는 SW를 누르면 EXTI가 발생하도록 설정한다.
 
 void SwEXTI_Config(void)
 {
 		// -- Nucleo Board Switch (PC13)설정
-		// -- <7-1> GPIOC의 클럭을 enable		
-		/*__HAL_RCC_GPIOC_CLK_ENABLE();	
+		// -- <7-1> GPIOC의 클럭을 enable
+		/*__HAL_RCC_GPIOC_CLK_ENABLE();
 
 		// -- <7-2> SW1의 동작 조건을 GPIO 입력으로 설정
 		GPIO_Init_Struct.Pin 		= GPIO_PIN_13;	// GPIO에서 사용할 PIN 설정
 		GPIO_Init_Struct.Mode 	= GPIO_MODE_IT_FALLING; 				// Input Floating 모드
 		GPIO_Init_Struct.Pull 		= GPIO_NOPULL; 						// Pull Up / Pull Down 비활성화
-		GPIO_Init_Struct.Speed 	= GPIO_SPEED_FREQ_HIGH; 		// 동작속도를 HIGH로 
+		GPIO_Init_Struct.Speed 	= GPIO_SPEED_FREQ_HIGH; 		// 동작속도를 HIGH로
 
-		// -- <7-3> GPIOC를 GPIO_Init_Struct에 설정된 조건으로 초기화함	
+		// -- <7-3> GPIOC를 GPIO_Init_Struct에 설정된 조건으로 초기화함
 		HAL_GPIO_Init(GPIOC, &GPIO_Init_Struct);
 
-		// -- 확장 IO Board Switch (PA8, PB4, PB5, PB10)설정	
+		// -- 확장 IO Board Switch (PA8, PB4, PB5, PB10)설정
 		//  SW1용 GPIO (PA8)의 초기설정
-		// -- <7-4> GPIOA의 클럭을 enable		
-		__HAL_RCC_GPIOA_CLK_ENABLE();	
+		// -- <7-4> GPIOA의 클럭을 enable
+		__HAL_RCC_GPIOA_CLK_ENABLE();
 
 		// -- <7-5> SW1의 동작 조건을 GPIO 입력으로 설정
 		GPIO_Init_Struct.Pin 		= GPIO_PIN_8;	// GPIO에서 사용할 PIN 설정
 		GPIO_Init_Struct.Mode 	= GPIO_MODE_IT_FALLING; 				// Input Floating 모드
 		GPIO_Init_Struct.Pull 		= GPIO_NOPULL; 						// Pull Up / Pull Down 비활성화
-		GPIO_Init_Struct.Speed 	= GPIO_SPEED_FREQ_HIGH; 		// 동작속도를 HIGH로 
+		GPIO_Init_Struct.Speed 	= GPIO_SPEED_FREQ_HIGH; 		// 동작속도를 HIGH로
 
-		// -- <7-6> GPIOA를 GPIO_Init_Struct에 설정된 조건으로 초기화함	
-		HAL_GPIO_Init(GPIOA, &GPIO_Init_Struct);    
+		// -- <7-6> GPIOA를 GPIO_Init_Struct에 설정된 조건으로 초기화함
+		HAL_GPIO_Init(GPIOA, &GPIO_Init_Struct);
 
 		//  SW2, SW3, SW4용 GPIO (PB4, PB5, PB10)의 초기설정
-		// -- <7-7> GPIOB의 클럭을 enable	
-		__HAL_RCC_GPIOB_CLK_ENABLE();	
-		
+		// -- <7-7> GPIOB의 클럭을 enable
+		__HAL_RCC_GPIOB_CLK_ENABLE();
+
 		// -- <7-8> SW의 동작 조건을 GPIO 입력으로 설정
 		GPIO_Init_Struct.Pin 		= GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_10;		// GPIO에서 사용할 PIN 설정
 		GPIO_Init_Struct.Mode 	= GPIO_MODE_IT_FALLING; 				// Input Floating 모드
 		GPIO_Init_Struct.Pull 		= GPIO_NOPULL; 						// Pull Up / Pull Down 비활성화
-		GPIO_Init_Struct.Speed 	= GPIO_SPEED_FREQ_HIGH; 		// 동작속도를 HIGH로 
+		GPIO_Init_Struct.Speed 	= GPIO_SPEED_FREQ_HIGH; 		// 동작속도를 HIGH로
 
-		// -- <7-9> GPIOB를 GPIO_Init_Struct에 설정된 조건으로 초기화함	
-		HAL_GPIO_Init(GPIOB, &GPIO_Init_Struct);       
-	
+		// -- <7-9> GPIOB를 GPIO_Init_Struct에 설정된 조건으로 초기화함
+		HAL_GPIO_Init(GPIOB, &GPIO_Init_Struct);
+
 		// -- <7-10> Sw1의 EXTI에 대한 인터럽트의 우선순위를 설정하고, 인터럽트를 enable함
 		HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);	 //  EXTI 의 priority를 설정
 		HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);			 //  EXTI 인터럽트를 enable
-	
-		HAL_NVIC_SetPriority(EXTI4_IRQn, 0, 0);		//  EXTI 의 priority를 설정	
+
+		HAL_NVIC_SetPriority(EXTI4_IRQn, 0, 0);		//  EXTI 의 priority를 설정
 		HAL_NVIC_EnableIRQ(EXTI4_IRQn);				//  EXTI 인터럽트를 enable
-		
+
 		HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);	//  EXTI 의 priority를 설정
 		HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);			//  EXTI 인터럽트를 enable
 		*/
-		
+
 }
 
 // -------------------------------------------------------------------------------- //
@@ -161,7 +158,7 @@ void TIM4_Config(int prescaler_value, int period_value)
 
 		// -- <8-1> TIM2의 clock을 enable
 		__HAL_RCC_TIM4_CLK_ENABLE();
-		
+
 		// -- <8-2> TIM의 동작 조건 설정 (인터럽트가 1초에 1번씩 발생하도록 주파수를 1 Hz로 설정)
 		TimHandle_4.Instance = TIM4;								// TIM2 사용
 		TimHandle_4.Init.CounterMode = TIM_COUNTERMODE_UP;	//	Up Counter 모드 설정
@@ -171,12 +168,12 @@ void TIM4_Config(int prescaler_value, int period_value)
 		TimHandle_4.Init.RepetitionCounter = 0;
 
 		// -- <8-3> TIM2를 TimHandle에 설정된 값으로 초기화하고 TIM를 동작
-		HAL_TIM_Base_Init(&TimHandle_4);	
-		HAL_TIM_Base_Start_IT(&TimHandle_4);	
-		
+		HAL_TIM_Base_Init(&TimHandle_4);
+		HAL_TIM_Base_Start_IT(&TimHandle_4);
+
 		// -- <8-4> 타이머 인터럽트 우선순위 설정 및 ENABLE
 		HAL_NVIC_SetPriority(TIM4_IRQn, 0, 0);		// 별도로 설정하지 않으면 디폴트로 설정됨
-		HAL_NVIC_EnableIRQ(TIM4_IRQn);		
+		HAL_NVIC_EnableIRQ(TIM4_IRQn);
 }
 
 void TIM2_Config(int prescaler_value, int period_value)
@@ -184,7 +181,7 @@ void TIM2_Config(int prescaler_value, int period_value)
 
 		// -- <8-1> TIM2의 clock을 enable
 		__HAL_RCC_TIM2_CLK_ENABLE();
-		
+
 		// -- <8-2> TIM의 동작 조건 설정 (인터럽트가 1초에 1번씩 발생하도록 주파수를 1 Hz로 설정)
 		TimHandle_2.Instance = TIM2;								// TIM2 사용
 		TimHandle_2.Init.CounterMode = TIM_COUNTERMODE_UP;	//	Up Counter 모드 설정
@@ -193,12 +190,12 @@ void TIM2_Config(int prescaler_value, int period_value)
 		TimHandle_2.Init.ClockDivision = 0;					// Clock Division 설정
 
 		// -- <8-3> TIM2를 TimHandle에 설정된 값으로 초기화하고 TIM를 동작
-		HAL_TIM_Base_Init(&TimHandle_2);	
-		HAL_TIM_Base_Start_IT(&TimHandle_2);	
-		
+		HAL_TIM_Base_Init(&TimHandle_2);
+		HAL_TIM_Base_Start_IT(&TimHandle_2);
+
 		// -- <8-4> 타이머 인터럽트 우선순위 설정 및 ENABLE
 		HAL_NVIC_SetPriority(TIM2_IRQn, 0, 0);		// 별도로 설정하지 않으면 디폴트로 설정됨
-		HAL_NVIC_EnableIRQ(TIM2_IRQn);		
+		HAL_NVIC_EnableIRQ(TIM2_IRQn);
 }
 
 
@@ -207,7 +204,7 @@ void TIM3_Config(int prescaler_value, int period_value)
 
 		// -- <9-1> TIM2의 clock을 enable
 		__HAL_RCC_TIM3_CLK_ENABLE();
-		
+
 		// -- <9-2> TIM의 동작 조건 설정 (인터럽트가 1초에 1번씩 발생하도록 주파수를 1 Hz로 설정)
 		TimHandle_3.Instance = TIM3;								// TIM3 사용
 		TimHandle_3.Init.CounterMode = TIM_COUNTERMODE_UP;	//	Down Counter 모드 설정
@@ -216,12 +213,12 @@ void TIM3_Config(int prescaler_value, int period_value)
 		TimHandle_3.Init.ClockDivision = 0;					// Clock Division 설정
 
 		// -- <9-3> TIM3를 TimHandle에 설정된 값으로 초기화하고 TIM를 동작
-		HAL_TIM_Base_Init(&TimHandle_3);	
-		HAL_TIM_Base_Start_IT(&TimHandle_3);	
-		
+		HAL_TIM_Base_Init(&TimHandle_3);
+		HAL_TIM_Base_Start_IT(&TimHandle_3);
+
 		// -- <9-4> 타이머 인터럽트 우선순위 설정 및 ENABLE
 		HAL_NVIC_SetPriority(TIM3_IRQn, 0, 0);		// 별도로 설정하지 않으면 디폴트로 설정됨
-		HAL_NVIC_EnableIRQ(TIM3_IRQn);		
+		HAL_NVIC_EnableIRQ(TIM3_IRQn);
 }
 
 
@@ -232,16 +229,16 @@ void TIM_OC_Config(TIM_HandleTypeDef handle_n, int pulse_value1,int pulse_value2
 		TIM_OCInit.Pulse = pulse_value1;							// Capture/Compare Register(CCR)의 설정값
 		TIM_OCInit.OCPolarity = TIM_OCPOLARITY_HIGH;			// OC 출력을 High로 설정
 		TIM_OCInit.OCFastMode = TIM_OCFAST_ENABLE; 		// TIM Output Compare Fast를 Disable
-	
+
 		HAL_TIM_OC_Init(&handle_n);
 		HAL_TIM_OC_ConfigChannel(&handle_n, &TIM_OCInit, TIM_CHANNEL_1);
 		HAL_TIM_OC_Start_IT(&handle_n,TIM_CHANNEL_1);
-	
+
 		TIM_OCInit.Pulse = pulse_value2;							// Capture/Compare Register(CCR)의 설정값
 		HAL_TIM_OC_Init(&handle_n);
 		HAL_TIM_OC_ConfigChannel(&handle_n, &TIM_OCInit, TIM_CHANNEL_2);
 		HAL_TIM_OC_Start_IT(&handle_n,TIM_CHANNEL_2);
-	
+
 		TIM_OCInit.Pulse = pulse_value3;							// Capture/Compare Register(CCR)의 설정값
 		HAL_TIM_OC_Init(&handle_n);
 		HAL_TIM_OC_ConfigChannel(&handle_n, &TIM_OCInit, TIM_CHANNEL_3);
@@ -254,11 +251,11 @@ void TIM_PWM_IT_Config(int pwm_value)
 {
 
 		/*// -- <11-1> TIM의 OC 동작조건설정
-		TIM_OCInit.OCMode = TIM_OCMODE_PWM1;					
-		TIM_OCInit.Pulse = pwm_value;											
-		TIM_OCInit.OCPolarity = TIM_OCPOLARITY_HIGH;						
+		TIM_OCInit.OCMode = TIM_OCMODE_PWM1;
+		TIM_OCInit.Pulse = pwm_value;
+		TIM_OCInit.OCPolarity = TIM_OCPOLARITY_HIGH;
 
-		// -- <11-2> TIM PWM를 TimHandle에 설정된 값으로 초기화 함 	
+		// -- <11-2> TIM PWM를 TimHandle에 설정된 값으로 초기화 함
 		HAL_TIM_PWM_Init(&TimHandle);
 
 		// -- <11-3> TIM PWM의 Channel을 TIM_OCInit에 설정된 값으로 초기화 함
@@ -275,11 +272,11 @@ void TIM_PWM_Poll_Config(int pwm_value)
 {
 
 		/*// -- <12-1> TIM의 OC 동작조건설정
-		TIM_OCInit.OCMode = TIM_OCMODE_PWM1;					
-		TIM_OCInit.Pulse = pwm_value;											
-		TIM_OCInit.OCPolarity = TIM_OCPOLARITY_HIGH;						
+		TIM_OCInit.OCMode = TIM_OCMODE_PWM1;
+		TIM_OCInit.Pulse = pwm_value;
+		TIM_OCInit.OCPolarity = TIM_OCPOLARITY_HIGH;
 
-		// -- <12-2> TIM PWM를 TimHandle에 설정된 값으로 초기화 함 	
+		// -- <12-2> TIM PWM를 TimHandle에 설정된 값으로 초기화 함
 		HAL_TIM_PWM_Init(&TimHandle);
 
 		// -- <12-3> TIM PWM의 Channel을 TIM_OCInit에 설정된 값으로 초기화 함
@@ -290,7 +287,7 @@ void TIM_PWM_Poll_Config(int pwm_value)
 }
 
 // -------------------------------------------------------------------------------- //
-// -- <13> UART의 초기설정을 위한 함수 
+// -- <13> UART의 초기설정을 위한 함수
 void UART2_Config(void)
 {
 	__HAL_RCC_GPIOA_CLK_ENABLE();
@@ -306,19 +303,19 @@ void UART2_Config(void)
 	GPIO_Init_Struct.Mode	= GPIO_MODE_INPUT;
 	GPIO_Init_Struct.Pull	= GPIO_NOPULL;
 	GPIO_Init_Struct.Speed	= GPIO_SPEED_FREQ_HIGH;
-	HAL_GPIO_Init(GPIOA, &GPIO_Init_Struct);	
+	HAL_GPIO_Init(GPIOA, &GPIO_Init_Struct);
 
-	UartHandle.Instance	= USART2;  
+	UartHandle.Instance	= USART2;
 	UartHandle.Init.BaudRate	= 9600;
 	UartHandle.Init.WordLength	= UART_WORDLENGTH_8B;
 	UartHandle.Init.StopBits	= UART_STOPBITS_1;
 	UartHandle.Init.Parity	= UART_PARITY_NONE;
 	UartHandle.Init.HwFlowCtl	= UART_HWCONTROL_NONE;
 	UartHandle.Init.Mode	= UART_MODE_TX_RX;
-	
+
 	HAL_UART_Init(&UartHandle);
 
-	//HAL_NVIC_SetPriority(USART2_IRQn, 0, 0);	
+	//HAL_NVIC_SetPriority(USART2_IRQn, 0, 0);
 	HAL_NVIC_EnableIRQ(USART2_IRQn);
 }
 
@@ -337,34 +334,34 @@ void UART3_B_Config(void)
 	GPIO_Init_Struct.Mode	= GPIO_MODE_INPUT;
 	GPIO_Init_Struct.Pull	= GPIO_NOPULL;
 	GPIO_Init_Struct.Speed	= GPIO_SPEED_FREQ_HIGH;
-	HAL_GPIO_Init(GPIOB, &GPIO_Init_Struct);	
+	HAL_GPIO_Init(GPIOB, &GPIO_Init_Struct);
 
-	UartHandle_B.Instance	= USART3;  
+	UartHandle_B.Instance	= USART3;
 	UartHandle_B.Init.BaudRate	= 9600;
 	UartHandle_B.Init.WordLength	= UART_WORDLENGTH_8B;
 	UartHandle_B.Init.StopBits	= UART_STOPBITS_1;
 	UartHandle_B.Init.Parity	= UART_PARITY_NONE;
 	UartHandle_B.Init.HwFlowCtl	= UART_HWCONTROL_NONE;
 	UartHandle_B.Init.Mode	= UART_MODE_TX_RX;
-	
+
 	HAL_UART_Init(&UartHandle_B);
 
-	//HAL_NVIC_SetPriority(USART3_IRQn, 0, 0);	
+	//HAL_NVIC_SetPriority(USART3_IRQn, 0, 0);
 	HAL_NVIC_EnableIRQ(USART3_IRQn);
 }
 
 // -------------------------------------------------------------------------------- //
-// -- <14> ADC의 초기설정을 위한 함수 
+// -- <14> ADC의 초기설정을 위한 함수
 void ADC1_Polling_Config(void)
-{	  
-    /*// -- <14-1> ADC의 clock을 활성화 
-    __HAL_RCC_GPIOA_CLK_ENABLE();	
+{
+    /*// -- <14-1> ADC의 clock을 활성화
+    __HAL_RCC_GPIOA_CLK_ENABLE();
     __HAL_RCC_ADC1_CLK_ENABLE();
-	
+
     GPIO_Init_Struct.Mode  = GPIO_MODE_ANALOG;
     GPIO_Init_Struct.Pin 	= GPIO_PIN_1;
-		GPIO_Init_Struct.Pull = GPIO_NOPULL;	
-    HAL_GPIO_Init(GPIOA, &GPIO_Init_Struct);	
+		GPIO_Init_Struct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &GPIO_Init_Struct);
 
     // -- <14-2> ADC의 동작 조건 설정
 		AdcHandler.Instance = ADC1;
@@ -373,14 +370,14 @@ void ADC1_Polling_Config(void)
 		AdcHandler.Init.ExternalTrigConv = ADC_SOFTWARE_START;
 		AdcHandler.Init.DataAlign = ADC_DATAALIGN_RIGHT;
 		AdcHandler.Init.NbrOfConversion = 1;
-	
-    // -- <14-3> ADC를 AdcHandler에 설정된 값으로 초기화 함 	
+
+    // -- <14-3> ADC를 AdcHandler에 설정된 값으로 초기화 함
     HAL_ADC_Init(&AdcHandler);
 
 	// -- <14-4> ADC Calibration을 시작
     HAL_ADCEx_Calibration_Start(&AdcHandler);
 
-    // -- <14-5> ADC Channel동작조건 설정	
+    // -- <14-5> ADC Channel동작조건 설정
     Adc_sConfig.Channel	= ADC_CHANNEL_1;
     Adc_sConfig.Rank         	= 1;
     Adc_sConfig.SamplingTime 	= ADC_SAMPLETIME_13CYCLES_5;
@@ -390,24 +387,24 @@ void ADC1_Polling_Config(void)
 
 		// -- <14-7> ADC를 동작시킴
 		HAL_ADC_Start(&AdcHandler);
-						
+
 		// -- <14-8> 변환이 완료될 때까지 대기
 		HAL_ADC_PollForConversion(&AdcHandler, 10);	*/
 
 }
 
 // -------------------------------------------------------------------------------- //
-// -- <15> ADC의 초기설정을 위한 함수 
+// -- <15> ADC의 초기설정을 위한 함수
 void ADC1_Interrupt_Config(void)
-{	  
-    /*// -- <15-1> ADC의 clock을 활성화 
-    __HAL_RCC_GPIOA_CLK_ENABLE();	
+{
+    /*// -- <15-1> ADC의 clock을 활성화
+    __HAL_RCC_GPIOA_CLK_ENABLE();
     __HAL_RCC_ADC1_CLK_ENABLE();
-	
+
     GPIO_Init_Struct.Mode  = GPIO_MODE_ANALOG;
     GPIO_Init_Struct.Pin 	= GPIO_PIN_1;
-		GPIO_Init_Struct.Pull = GPIO_NOPULL;	
-    HAL_GPIO_Init(GPIOA, &GPIO_Init_Struct);	
+		GPIO_Init_Struct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &GPIO_Init_Struct);
 
     // -- <15-2> ADC의 동작 조건 설정
 		AdcHandler.Instance = ADC1;
@@ -416,14 +413,14 @@ void ADC1_Interrupt_Config(void)
 		AdcHandler.Init.ExternalTrigConv = ADC_SOFTWARE_START;
 		AdcHandler.Init.DataAlign = ADC_DATAALIGN_RIGHT;
 		AdcHandler.Init.NbrOfConversion = 1;
-	
-    // -- <15-3> ADC를 AdcHandler에 설정된 값으로 초기화 함 	
+
+    // -- <15-3> ADC를 AdcHandler에 설정된 값으로 초기화 함
     HAL_ADC_Init(&AdcHandler);
 
 	// -- <15-4> ADC Calibration을 시작
     HAL_ADCEx_Calibration_Start(&AdcHandler);
 
-    // -- <15-5> ADC Channel동작조건 설정	
+    // -- <15-5> ADC Channel동작조건 설정
     Adc_sConfig.Channel	= ADC_CHANNEL_1;
     Adc_sConfig.Rank         	= 1;
     Adc_sConfig.SamplingTime 	= ADC_SAMPLETIME_13CYCLES_5;
@@ -433,9 +430,9 @@ void ADC1_Interrupt_Config(void)
 
 		// -- <15-7> ADC를 동작시킴
 		HAL_ADC_Start_IT(&AdcHandler);
-	
-    // -- <15-7> ADC 인터럽트 우선순위 설정 및 활성화 
-    HAL_NVIC_SetPriority(ADC1_IRQn, 0, 0); 
+
+    // -- <15-7> ADC 인터럽트 우선순위 설정 및 활성화
+    HAL_NVIC_SetPriority(ADC1_IRQn, 0, 0);
     HAL_NVIC_EnableIRQ(ADC1_IRQn);*/
 
 }
@@ -462,7 +459,7 @@ void  SystemClock_Config(void)
 {
 		  RCC_ClkInitTypeDef clkinitstruct = {0};
 		  RCC_OscInitTypeDef oscinitstruct = {0};
-		  
+
 		  /* Configure PLL ------------------------------------------------------*/
 		  /* PLL configuration: PLLCLK = (HSI / 2) * PLLMUL = (8 / 2) * 16 = 64 MHz */
 		  /* PREDIV1 configuration: PREDIV1CLK = PLLCLK / HSEPredivValue = 64 / 1 = 64 MHz */
@@ -476,23 +473,23 @@ void  SystemClock_Config(void)
 		  oscinitstruct.PLL.PLLState    = RCC_PLL_ON;
 		  oscinitstruct.PLL.PLLSource   = RCC_PLLSOURCE_HSI_DIV2;
 		  oscinitstruct.PLL.PLLMUL      = RCC_PLL_MUL16;
-  
+
 		  if (HAL_RCC_OscConfig(&oscinitstruct)!= HAL_OK)   {
 					/* Initialization Error */
-					while(1); 
+					while(1);
 		  }
 
-		  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+		  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
 			 clocks dividers */
 		  clkinitstruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
 		  clkinitstruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
 		  clkinitstruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
 		  clkinitstruct.APB2CLKDivider = RCC_HCLK_DIV1;
-		  clkinitstruct.APB1CLKDivider = RCC_HCLK_DIV2;  
-		  
+		  clkinitstruct.APB1CLKDivider = RCC_HCLK_DIV2;
+
 		  if (HAL_RCC_ClockConfig(&clkinitstruct, FLASH_LATENCY_2)!= HAL_OK)   {
 					/* Initialization Error */
-					while(1); 
+					while(1);
 		  }
 }
 
@@ -513,7 +510,7 @@ void assert_failed(uint8_t *file, uint32_t line)
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
 
 		  /* Infinite loop */
-		  while (1) 
+		  while (1)
 		  {
 		  }
 }
--- Src/stm32f1xx_it.c
+++ Src/stm32f1xx_it.c
@@ -1,16 +1,3 @@
-
-/*******************************************************************************
-  
-  * File name :  TIM_4 / stm32f1xx_it.c  
-  
-			* Application model : Nucleo_F103 Board  
- 
-			* Ver4.0
-			* January 23. 2017
-			* by Sang-min Kim, Jae-Il Kim  &  Dong-hyuk Cha
- 
- ****************************************************************************** */
-
 #include "main.h"
 #include "stm32f1xx_it.h"					// 인터럽트 사용에 필요한 헤더 파일
 
